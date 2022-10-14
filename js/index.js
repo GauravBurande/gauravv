8e7117e03c8ddc941a60a70c08092549faea5860
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-10-14 23:58:45 +0600

    added - home page

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var today = new Date();
+var year = today.getFullYear();
+
+var elYear = document.getElementById('year');
+elYear.textContent = year;
+
+// function setup() {
+    var email = document.getElementById('email');
+    email.focus();
+// }
+
+// window.addEventListener('load', setup, false);
+// setup();
+
+
+
+
+
+
+
